ec22763b3b09598504c2aa304a1ea3148c987787
afiq-codes26 <afiqaiman.asyahiran@gmail.com>
2024-05-13 10:49:50 +0800

    New Files added!

--- post_booking.php
+++ post_booking.php
@@ -1,29 +1,28 @@
 <html>
-<link rel="stylesheet" href="forms.css">
+<link rel="stylesheet" href="style/post_booking.css">
 <body>
-<table>
 <?php
-	session_start();
-	include("connection.php");
+  session_start();
+  include("connection.php");
 ?>
 
 <link rel="stylesheet" href="forms.css">
 
 <body>
 <center>
-<caption><h2>YOUR BOOKING IS SUCCESSFUL</h2>
-<h4>here are the details of your booking,please be present to avoid any problems</h4></caption>
+<h2>YOUR BOOKING IS SUCCESSFUL</h2>
+<h4>Here are the details for your booked lesson! Please attend to avoid any further inconveniences! </h4>
+<center>
 
-<table>
 <?php 
-	$Student_ID=$_SESSION['Student_ID'];
-	$sql = "select * from booking_db
+  $Student_ID= $_SESSION['Username'];
+  $sql = "select * from booking_db
         join subject_db on booking_db.Subject_ID = subject_db.Subject_ID
         JOIN tutor_db on booking_db.Tutor_ID = tutor_db.Tutor_ID
         join allocation_db on booking_db.Allocation_ID = allocation_db.Allocation_ID
-		join student_db on booking_db.Student_ID = student_db.Student_ID
+    join student_db on booking_db.Student_ID = student_db.Student_ID
         where booking_db.Student_ID = '$Student_ID'
-		ORDER BY booking_db.Booking_ID DESC
+    ORDER BY booking_db.Booking_ID DESC
         limit 1";
       
         $data = mysqli_query($connection, $sql);
@@ -34,18 +33,15 @@
       } else {
       // If the query executed successfully, proceed with fetching and displaying the data
       while ($result = mysqli_fetch_assoc($data)) {
-        echo "<tr>". $result['Subject_Name'] ."</tr>
-        <br>
-        <tr>" . $result['Tutor_Name'] . "</tr>
-        <br>
-		<tr>" . $result['Timeslot'] . "</tr>
-		<br>
-		<tr>" . $result['Booking_Date'] . "</tr>";
-
+        echo "<div> ".$result['Subject_Name']."</div>";
+		echo "<div>". $result['Tutor_Name'] ."</div>";
+		echo "<div>". $result['Timeslot'] ."</div>";
+		echo "<div>". $result['Booking_Date']. "</div>";
       }
     }
     ?>
-</table>	
+
+</center> 
+<a href = "booking.php"> Return to Homepage! </a>
 </center>
 </body>
-
